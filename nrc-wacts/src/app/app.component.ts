b3d2c305b96e13d3fd05b8dff046c7223d2ea7a1
Syed Zaidi <yknse27@gmail.com>
2025-05-19 00:07:33 -0400

    started project setup, top section skeleton complete

---
+++ nrc-wacts/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'NRC WACTS';
+}
